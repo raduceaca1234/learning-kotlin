1b535f51c0d3b0c50995092a1a4b006e11f76aa5
raduceaca1234 <ceaca.radu@yahoo.com>
2020-10-12 02:58:56 +0300

    Working with collections

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -11,6 +11,11 @@ fun main(){
 //    sayHello()
     val interestingThings = arrayOf("Item1", "Item2", "Item3")
     val interestingThings2 = listOf("Item1", "Item2", "Item3")
+    val interestingThings3 = mutableListOf("Item1", "Item2", "Item3")
+    interestingThings3.add("Dogs")
+    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
+    val map2 = mutableMapOf(1 to "a", 2 to "b", 3 to "c")
+    map2.put(4, "d")
     println(interestingThings.size)
     println(interestingThings[0])
     println(interestingThings.get(0))
@@ -22,5 +27,9 @@ fun main(){
     interestingThings2.forEach{interestingThing->
         println(interestingThing)
     }
+
+    map.forEach{key, value -> println("$key -> $value")}
+
+
 
 }
