fde6971d651c12a3129c014267868de4758cffe8
raduceaca1234 <ceaca.radu@yahoo.com>
2020-10-12 02:52:03 +0300

    Working with Arrays

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -6,7 +6,15 @@ fun sayHello() {
 }
 
 fun main(){
-    println("Hello World")
-    println(getGreeting())
-    sayHello()
+//    println("Hello World")
+//    println(getGreeting())
+//    sayHello()
+    val interestingThings = arrayOf("Item1", "Item2", "Item3")
+    println(interestingThings.size)
+    println(interestingThings[0])
+    println(interestingThings.get(0))
+
+    interestingThings.forEachIndexed{index, s ->
+        println("$s is at index $index")
+    }
 }
